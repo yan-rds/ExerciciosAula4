367bd1d04eb1c3f3f534724cddf6f8becd6ca3bc
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-20 15:30:10 -0300

    Pasta para cada pais criada

--- Exercicio3.bat
+++ Exercicio3.bat
@@ -1,1 +1,3 @@
 md "Todos os Paises"
+cd "Todos os paises"
+md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Guiana Francesa" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela"
