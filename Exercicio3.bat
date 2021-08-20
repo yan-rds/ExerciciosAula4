a1b2005851a3df786b4fb3612360495b58451056
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-20 15:34:38 -0300

    Todos os Estados do Brasil adicionados

--- Exercicio3.bat
+++ Exercicio3.bat
@@ -1,3 +1,5 @@
 md "Todos os Paises"
 cd "Todos os paises"
 md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Guiana Francesa" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela"
+cd "Brasil"
+md "Acre" "Alagoas" "Amapa" "Amazonas" "Bahia" "Ceara" "Distrito Federal" "Espirito Santo" "Goias" "Maranhao" "Mato Grosso" "Mato Grosso do Sul" "Minas Gerais" "Parana" "Paraiba" "Para" "Pernambuco" "Piaui" "Rio Grande do Norte" "Rio Grande do Sul" "Rio de Janeiro" "Rondonia" "Roraima" "Santa Catarina" "Sergipe" "Sao Paulo" "Tocantins"
