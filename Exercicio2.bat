2255818f627ff4026b683c735af212fbdb77fdf8
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-20 15:13:25 -0300

    Estados do norte Adicionados

--- Exercicio2.bat
+++ Exercicio2.bat
@@ -6,3 +6,6 @@ md "Goias" "Mato Grosso" "Mato Grosso do Sul" "Distrito Federal"
 cd ..
 cd "Nordeste"
 md "Maranhao" "Piaui" "Ceara" "Rio Grande do norte" "Paraiba" "Pernambuco" "Alagoas" "Sergipe" "Bahia"
+cd ..
+cd "Norte"
+md "Acre" "Amapa" "Amazonas" "Para" "Rondonia" "Roraima" "Tocantins"
