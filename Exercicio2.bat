9aeeb394455bfe94dac6483facb0fed89f7309d1
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-20 15:10:25 -0300

    Estados do nordeste adicionados

--- Exercicio2.bat
+++ Exercicio2.bat
@@ -4,3 +4,5 @@ md "Norte" "Sul" "Nordeste" "Sudeste" "Centro-Oeste"
 cd "Centro-Oeste"
 md "Goias" "Mato Grosso" "Mato Grosso do Sul" "Distrito Federal"
 cd ..
+cd "Nordeste"
+md "Maranhao" "Piaui" "Ceara" "Rio Grande do norte" "Paraiba" "Pernambuco" "Alagoas" "Sergipe" "Bahia"
