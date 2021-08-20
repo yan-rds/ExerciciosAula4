75e11c71a50e948639ecaaebe1344531abcd8cd9
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-20 14:44:25 -0300

    Todos os Clubes Serie A

--- Exercicio 1.bat
+++ Exercicio 1.bat
@@ -4,3 +4,6 @@ md "Serie A"
 md "Serie B"
 cd "Serie A"
 md "Atletico-MG" "Palmeiras" "Fortaleza" "Bragantino" "Flamengo" "Athletico -PR" "Atletico-GO" "Ceará" "Internacional" "Santos" "Corinthians" "Juventude" "Bahia" "Sao Paulo" "Fluminense" "Cuiabá" "Sport" "America-MG" "Gremio" "Chapecoense" 
+cd ..
+cd "Serie B"
+md "Curitiba" "Goias" "Avai" "CRB" "Sampaio Correa" "Guarani" "Nautico" "Botafogo" "Operario-PR" "CSA" "Vasco" "Remo" "Brusque" "Cruzeiro" "Vila Nova" "Ponte Preta" "Londrina" "Vitoria" "Confianca" "Brasil de Pelotas"
